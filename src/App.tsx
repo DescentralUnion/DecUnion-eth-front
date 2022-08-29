031484fee8b0da25c0c0533ed1786fff168f976e
mgutierrezo <martin72.gutierrez@gmail.com>
2022-08-29 17:58:14 -0500

    Added wallet connect functionality, address and balance now showing

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import logo from "./logo.svg"
 import "./App.css"
 import Home from "./components/home/Home"
 import ConnectWallet from "./components/connectWallet/ConnectWallet"
+import { BigNumber } from "ethers"
 
 function App() {
   const [isConnected, setIsConnected] = useState<boolean>(false)
+  const [walletAddress, setWalletAddress] = useState<string>('')
+  const [walletBalance, setWalletBalance] = useState<string>('')
   
   return (
     <div className="App">
-      {isConnected ? (
-        <Home />
+      {walletAddress != '' ? (
+        <Home walletAddress={walletAddress} walletBalance={walletBalance} />
       ) : (
-        <ConnectWallet setIsConnected={setIsConnected} />
+        <ConnectWallet setIsConnected={setIsConnected} setWalletAddress={setWalletAddress} setWalletBalance={setWalletBalance} />
       )}
     </div>
   )
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,15 +1,24 @@
+import { BigNumber } from "ethers"
 import { useState } from "react"
 import Deposit from "./components/deposit/Deposit"
 import Statistics from "./components/statistics/Statistics"
 import Withdraw from "./components/withdraw/Withdraw"
 import './home.css'
 
-const Home = () => {
+interface HomeProps {
+  walletAddress: string
+  walletBalance: string
+}
+const Home = ({walletAddress, walletBalance}: HomeProps) => {
   const [showDeposit, setShowDeposit] = useState<boolean>(true)
 
   return (
     <div className="homeWrapper">
       <div className="panelWrapper">
+        <div className="infoWrapper">
+          {walletAddress != '' ? <span>{walletAddress}</span> : null}
+          {walletBalance != undefined ?  <span>{walletBalance}</span> : null}
+        </div>
         <div className="selectorWrapper">
           <span className={`selectorButton ${!showDeposit ? "selectorButton-opacity" : ""}`} onClick={() => setShowDeposit(true)}>Deposit</span>
           <span className={`selectorButton ${showDeposit ? "selectorButton-opacity" : ""}`} onClick={() => setShowDeposit(false)}>Withdraw</span>
